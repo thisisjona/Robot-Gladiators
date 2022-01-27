45c202dd2eedd190c5e39804d7f73b5e10c7c944
thisisjona <thisisjona@gmail.com>
2022-01-26 18:50:36 -0600

    faulty while loop

--- assets/js/game.js
+++ assets/js/game.js
@@ -13,8 +13,7 @@ var enemyAttack = 12;
 
 // fight function
 var fight = function(enemyName) {
-  // Alert players that they are starting the round
-  window.alert("Welcome to Robot Gladiators!");
+    while(enemyHealth > 0){
 
   // ask player if they'd like to fight or run
   var promptFight = window.prompt('Would you like to FIGHT or SKIP this battle? Enter "FIGHT" or "SKIP" to choose.');
@@ -24,20 +23,20 @@ var fight = function(enemyName) {
     // remove enemy's health by subtracting the amount set in the playerAttack variable
     enemyHealth = enemyHealth - playerAttack;
     console.log(
-      playerName + " attacked " + + ". " + + " now has " + enemyHealth + " health remaining."
+      playerName + " attacked " + enemyName + ". " + enemyName + " now has " + enemyHealth + " health remaining."
     );
 
     // check enemy's health
     if (enemyHealth <= 0) {
-      window.alert(+ " has died!");
+      window.alert(enemyName + " has died!");
     } else {
-      window.alert(+ " still has " + enemyHealth + " health left.");
+      window.alert(enemyName + " still has " + enemyHealth + " health left.");
     }
 
     // remove players's health by subtracting the amount set in the enemyAttack variable
     playerHealth = playerHealth - enemyAttack;
     console.log(
-      + " attacked " + playerName + ". " + playerName + " now has " + playerHealth + " health remaining."
+     enemyName + " attacked " + playerName + ". " + playerName + " now has " + playerHealth + " health remaining."
     );
 
     // check player's health
@@ -64,9 +63,12 @@ var fight = function(enemyName) {
   } else {
     window.alert("You need to pick a valid option. Try again!");
   }
+    }
+
 };
 
 // run fight function to start game
 for(var i = 0; i < enemyNames.length; i++){
-    fight(enemyNames[i])
+    var pickedEnemyName = enemyNames[i];
+    fight(pickedEnemyName);
 };
